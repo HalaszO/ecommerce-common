258eefe0e846e9214746d6a40f6aa3f5e1a36e4b
Olivér Halász <oliver.viktor.halasz@gmail.com>
2021-01-30 16:48:29 +0100

    Renamed error

--- src/classes/errors/ResourceNotFoundError.ts
+++ src/classes/errors/ResourceNotFoundError.ts
@@ -1,11 +1,11 @@
 import { CustomError } from "./CustomError";
 
-export class RouteNotFoundError extends CustomError {
+export class ResourceNotFoundError extends CustomError {
   statusCode = 404;
 
   constructor() {
     super("Route not found");
-    // Object.setPrototypeOf(this, RouteNotFoundError.prototype);
+    // Object.setPrototypeOf(this, ResourceNotFoundError.prototype);
   }
 
   serializeErrors() {
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from "./classes/errors/CustomError";
 export * from "./classes/errors/DBConnectionError";
 export * from "./classes/errors/NotAuthorizedError";
 export * from "./classes/errors/RequestValidationError";
-export * from "./classes/errors/RouteNotFoundError";
+export * from "./classes/errors/ResourceNotFoundError";
 
 export * from "./middlewares/currentUser";
 export * from "./middlewares/errorHandler";
